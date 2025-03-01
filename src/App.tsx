738a1b2fde16d4f5e7d9dca02f97a1da5d7b6b62
stackdump <myork@stackdump.com>
2025-03-01 09:40:44 -0600

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -53,64 +53,6 @@ type Arrow = {
     inhibit?: boolean;
 }
 
-// Example Export
-//
-// Base64 encoded version of the model:
-//
-// https://pflow.dev/?b=eyJtb2RlbFR5cGUiOiJwZXRyaU5ldCIsInZlcn
-// Npb24iOiJ2MCIsInBsYWNlcyI6eyJwbGFjZTAiOnsiaW5pdGlhbCI6MSwiY
-// 2FwYWNpdHkiOjMsIm9mZnNldCI6MCwieCI6MTMwLCJ5IjoyMDd9LCJwbGFj
-// ZTEiOnsiaW5pdGlhbCI6MCwiY2FwYWNpdHkiOjAsIm9mZnNldCI6MSwieCI
-// 6Mzk1LCJ5IjoyOTl9fSwidHJhbnNpdGlvbnMiOnsidHhuMCI6eyJ4Ijo0Ni
-// wieSI6MTE2fSwidHhuMSI6eyJ4IjoyMjcsInkiOjExMn0sInR4bjIiOnsie
-// CI6NDMsInkiOjMwN30sInR4bjMiOnsieCI6MjM1LCJ5IjozMDZ9fSwiYXJj
-// cyI6W3sic291cmNlIjoidHhuMCIsInRhcmdldCI6InBsYWNlMCIsImluaGl
-// iaXQiOmZhbHNlLCJ3ZWlnaHQiOjF9LHsic291cmNlIjoicGxhY2UwIiwidG
-// FyZ2V0IjoidHhuMSIsImluaGliaXQiOmZhbHNlLCJ3ZWlnaHQiOjN9LHsic
-// 291cmNlIjoidHhuMiIsInRhcmdldCI6InBsYWNlMCIsImluaGliaXQiOnRy
-// dWUsIndlaWdodCI6M30seyJzb3VyY2UiOiJwbGFjZTAiLCJ0YXJnZXQiOiJ
-// 0eG4zIiwiaW5oaWJpdCI6dHJ1ZSwid2VpZ2h0IjoxfSx7InNvdXJjZSI6In
-// R4bjMiLCJ0YXJnZXQiOiJwbGFjZTEiLCJpbmhpYml0IjpmYWxzZSwid2VpZ2h0IjoxfV19
-//
-// Compare with brotli encoded version
-// https://pflow.dev/?z=G5sCIBwHdqMPWUYyo7XgaT/B/8+NT3WcvPDeu7
-// qxUgxFDCqCK/KRBeEniEJf6rAojNxceB5GeWQPKMNzjxcXnwuSeCTptkKoH
-// kH1utxupuM/CaRc5//wu8bbl2twEmQhzLGvitYK4NqGLIG0KVhGSHPwjPDE
-// Bsr6bgZZxvkMq+kpudx1FKU2E60lQT4IcSmuGd8zZHQ6lUa6F38vhvcRtbP
-// iGUyoBLLKVK7ZnkVntb+nQGiV3JXEpBDbjFzleFhtrgHy9XrJJJArRQE8hV7/iz/WJuePvvvRh1RRlyl5Lxhk7z01sfNo1dY=
-//
-// MinUrl encoded version
-//
-// &p= starts a new place
-//  &o= sets the offset of the place
-//  &i= sets the initial marking of the place
-//  &c= sets the capacity of the place
-//  &x= sets the x coordinate of the place
-//  &y= sets the y coordinate of the place
-//
-// &t= starts a new transition
-//  &o= sets the offset of the transition
-//  &r= sets the role of the transition
-//  &x= sets the x coordinate of the transition
-//  &y= sets the y coordinate of the transition
-//
-// &s= starts a new arc (source)
-//   &e= ends the arc (target)
-//   &w= sets the weight of the arc
-//   &n= sets the arc to be an inhibitor arc
-//
-// http://localhost:3000/?m=petriNet&v=v0
-// &p=place0&o=0&i=1&c=3&x=130&y=207
-// &p=place1&o=1&x=395&y=299
-// &t=txn0&x=46&y=116
-// &t=txn1&x=227&y=112
-// &t=txn2&x=43&y=307
-// &t=txn3&x=235&y=306
-// &s=txn0&e=place0
-// &s=place0&e=txn1
-// &s=txn2&e=place0
-// &s=place0&e=txn3
-// &s=txn3&e=place1
 function exportAsMinUrl(model: any): string {
     const params = new URLSearchParams();
 
@@ -198,64 +140,74 @@ function importFromMinUrl(url: string): Model {
     const queryString = url.split('?')[1];
     const params = queryString.split('&');
 
-    const model = {
+    const model: Model = {
         modelType: '',
         version: '',
-        places: {} as Record<string, Place>,
-        transitions: {} as Record<string, Transition>,
-        arcs: [] as Array<Arrow>
-    } as Model;
+        places: {},
+        transitions: {},
+        arcs: []
+    };
 
     let currentPlace: string | null = null;
     let currentTransition: string | null = null;
     let currentArc: number | null = null;
 
-    for (const param of params) {
+    params.forEach(param => {
         const [key, value] = param.split('=');
         const decodedValue = decodeURIComponent(value);
 
-        if (key === 'm') {
-            model.modelType = decodedValue;
-        } else if (key === 'v') {
-            model.version = decodedValue;
-        } else if (key === 'p') {
-            currentPlace = decodedValue;
-            model.places[currentPlace] = {initial: 0, capacity: 0} as Place;
-        } else if (!!currentPlace && key === 'o') {
-                model.places[currentPlace].offset = Number(decodedValue);
-        } else if (!!currentPlace && key === 'i') {
-            model.places[currentPlace]["initial"] = isNaN(Number(decodedValue)) ? 0 : Number(decodedValue);
-        } else if (!!currentPlace && key === 'c') {
-            model.places[currentPlace]["capacity"] = isNaN(Number(decodedValue)) ? 0 : Number(decodedValue);
-        }  else if (!!currentPlace && key === 'x') {
-            model.places[currentPlace].x = Number(decodedValue);
-        } else if (!!currentPlace && key === 'y') {
-            model.places[currentPlace].y = Number(decodedValue);
-        } else if (key === 't') {
-            currentPlace = null;
-            currentTransition = decodedValue;
-            model.transitions[currentTransition] = {} as Transition;
-        }  else if (!!currentTransition && key === 'x') {
-            model.transitions[currentTransition].x = Number(decodedValue);
-        } else if (!!currentTransition && key === 'y') {
-            model.transitions[currentTransition].y = Number(decodedValue);
-        } else if (key === 's') {
-            currentPlace = null;
-            currentTransition = null;
-            currentArc = model.arcs.length;
-            model.arcs.push({source: decodedValue, target: '', inhibit: false, weight: 1});
-        } else if (currentArc !== null && key === 'e') {
-            model.arcs[currentArc].target = decodedValue;
-        } else if (currentArc !== null && key === 'n') {
-            model.arcs[currentArc].inhibit = decodedValue === "1";
-        } else if (currentArc !== null && key === 'w') {
-            model.arcs[currentArc].weight = Number(decodedValue)
-        } else {
-            console.log({key, value, decodedValue, currentArc, currentTransition, currentPlace, msg: "unmatched"})
+        switch (key) {
+            case 'm':
+                model.modelType = decodedValue;
+                break;
+            case 'v':
+                model.version = decodedValue;
+                break;
+            case 'p':
+                currentPlace = decodedValue;
+                model.places[currentPlace] = {initial: 0, capacity: 0} as Place;
+                break;
+            case 'o':
+                if (currentPlace) model.places[currentPlace].offset = Number(decodedValue);
+                break;
+            case 'i':
+                if (currentPlace) model.places[currentPlace].initial = Number(decodedValue) || 0;
+                break;
+            case 'c':
+                if (currentPlace) model.places[currentPlace].capacity = Number(decodedValue) || 0;
+                break;
+            case 'x':
+                if (currentPlace) model.places[currentPlace].x = Number(decodedValue);
+                else if (currentTransition) model.transitions[currentTransition].x = Number(decodedValue);
+                break;
+            case 'y':
+                if (currentPlace) model.places[currentPlace].y = Number(decodedValue);
+                else if (currentTransition) model.transitions[currentTransition].y = Number(decodedValue);
+                break;
+            case 't':
+                currentPlace = null;
+                currentTransition = decodedValue;
+                model.transitions[currentTransition] = {} as Transition;
+                break;
+            case 's':
+                currentPlace = null;
+                currentTransition = null;
+                currentArc = model.arcs.length;
+                model.arcs.push({source: decodedValue, target: '', inhibit: false, weight: 1});
+                break;
+            case 'e':
+                if (currentArc !== null) model.arcs[currentArc].target = decodedValue;
+                break;
+            case 'n':
+                if (currentArc !== null) model.arcs[currentArc].inhibit = decodedValue === "1";
+                break;
+            case 'w':
+                if (currentArc !== null) model.arcs[currentArc].weight = Number(decodedValue);
+                break;
+            default:
+                break;
         }
-    }
-
-    console.log(JSON.stringify(model));
+    });
 
     return model;
 }
@@ -266,26 +218,32 @@ function exportAsUrl(model: any): string {
     params.append('modelType', model.modelType);
     params.append('version', model.version);
 
-    for (const [placeName, placeData] of Object.entries(model.places as Record<string, Place>)) {
+    Object.entries(model.places).forEach(([placeName, placeData]) => {
         params.append('place', placeName);
-        for (const [key, value] of Object.entries(placeData)) {
-            params.append(key, value.toString());
-        }
-    }
-
-    for (const [transitionName, transitionData] of Object.entries(model.transitions as Record<string, Transition>)) {
-        params.append('transition', transitionName);
-        for (const [key, value] of Object.entries(transitionData)) {
-            params.append(key, value.toString());
-        }
-    }
-
-    for (const arc of model.arcs) {
-        for (const [key, value] of Object.entries(arc)) {
+        // @ts-ignore
+        Object.entries(placeData).forEach(([key, value]) => {
             // @ts-ignore
             params.append(key, value.toString());
-        }
-    }
+        });
+    });
+
+    Object.entries(model.transitions).forEach(([transitionName, transitionData]) => {
+        params.append('transition', transitionName);
+        // @ts-ignore
+        Object.entries(transitionData).forEach(([key, value]) => {
+            // @ts-ignore
+            params.append(key, value.toString());
+        });
+    });
+
+    // @ts-ignore
+    model.arcs.forEach(arc => {
+        // @ts-ignore
+        Object.entries(arc).forEach(([key, value]) => {
+            // @ts-ignore
+            params.append(key, value.toString());
+        });
+    });
 
     return `?${params.toString()}`;
 }
@@ -302,63 +260,68 @@ function importUrl(url: string): Model {
 
 function importFromUrl(url: string): Model {
     const queryString = url.split('?')[1];
-    const params = queryString.split('&');
+    const params = new URLSearchParams(queryString);
 
-    const model = {
+    const model: Model = {
         modelType: '',
         version: '',
-        places: {} as Record<string, Place>,
-        transitions: {} as Record<string, Transition>,
-        arcs: [] as Array<Arrow>
-    } as Model;
+        places: {},
+        transitions: {},
+        arcs: []
+    };
 
     let currentPlace: string | null = null;
     let currentTransition: string | null = null;
     let currentArc: number | null = null;
 
-    for (const param of params) {
-        const [key, value] = param.split('=');
+    params.forEach((value, key) => {
         const decodedValue = decodeURIComponent(value);
 
-        if (key === 'modelType') {
-            model.modelType = decodedValue;
-        } else if (key === 'version') {
-            model.version = decodedValue;
-        } else if (key === 'place') {
-            currentTransition = null;
-            currentArc = null;
-            currentPlace = decodedValue;
-            model.places[currentPlace] = {initial: 0, capacity: 0 } as Place;
-        } else if (key === 'transition') {
-            currentPlace = null;
-            currentArc = null;
-            currentTransition = decodedValue;
-            model.transitions[currentTransition] = {} as Transition;
-        } else if (key === 'source') {
-            currentPlace = null;
-            currentTransition = null;
-            currentArc = model.arcs.length;
-            model.arcs.push({source: decodedValue, target: '', inhibit: false, weight: 1});
-        } else if (key === 'target') {
-            currentPlace = null;
-            currentTransition = null;
-            model.arcs[currentArc as number].target = decodedValue;
-        } else if (currentArc && model.arcs[currentArc]) {
-            if (key === 'inhibit') {
-                // @ts-ignore
-                model.arcs[currentArc].inhibit = decodedValue === 'true';
-            } else {
-                // @ts-ignore
-                model.arcs[currentArc][key as keyof Arrow] = isNaN(Number(decodedValue)) ? decodedValue : Number(decodedValue);
-            }
-        } else if (currentPlace && model.places[currentPlace]) {
-            // @ts-ignore
-            model.places[currentPlace][key as keyof Place] = isNaN(Number(decodedValue)) ? decodedValue : Number(decodedValue);
-        } else if (currentTransition && model.transitions[currentTransition]) {
-            // @ts-ignore
-            model.transitions[currentTransition][key as keyof Transition] = isNaN(Number(decodedValue)) ? decodedValue : Number(decodedValue);
+        switch (key) {
+            case 'modelType':
+                model.modelType = decodedValue;
+                break;
+            case 'version':
+                model.version = decodedValue;
+                break;
+            case 'place':
+                currentPlace = decodedValue;
+                currentTransition = null;
+                currentArc = null;
+                model.places[currentPlace] = {initial: 0, capacity: 0} as Place;
+                break;
+            case 'transition':
+                currentTransition = decodedValue;
+                currentPlace = null;
+                currentArc = null;
+                model.transitions[currentTransition] = {} as Transition;
+                break;
+            case 'source':
+                currentArc = model.arcs.length;
+                currentPlace = null;
+                currentTransition = null;
+                model.arcs.push({source: decodedValue, target: '', inhibit: false, weight: 1});
+                break;
+            case 'target':
+                if (currentArc !== null) model.arcs[currentArc].target = decodedValue;
+                break;
+            case 'inhibit':
+                if (currentArc !== null) model.arcs[currentArc].inhibit = decodedValue === 'true';
+                break;
+            default:
+                if (currentArc !== null) {
+                    // @ts-ignore
+                    model.arcs[currentArc][key as keyof Arrow] = isNaN(Number(decodedValue)) ? decodedValue : Number(decodedValue);
+                } else if (currentPlace) {
+                    // @ts-ignore
+                    model.places[currentPlace][key as keyof Place] = isNaN(Number(decodedValue)) ? decodedValue : Number(decodedValue);
+                } else if (currentTransition) {
+                    // @ts-ignore
+                    model.transitions[currentTransition][key as keyof Transition] = isNaN(Number(decodedValue)) ? decodedValue : Number(decodedValue);
+                }
+                break;
         }
-    }
+    });
 
     return model;
 }
@@ -385,7 +348,7 @@ function EmbeddedImport(): React.ReactElement {
         <a href={toUrl(imported)} target="_blank" rel="noreferrer">
             <img src={toImage(imported)} alt="test"/>
         </a>
-        <br />
+        <br/>
         <a href={exportAsUrl(imported)}>
             Re-encoded Url model
         </a>
@@ -405,13 +368,13 @@ function App() {
                 <a href={toUrl(model)} target="_blank" rel="noreferrer">
                     <img src={toImage(model)} alt="test"/>
                 </a>
-                <br />< br/>
+                <br/>< br/>
                 <a href={"?"}> Back &lt;- </a>
-                <br />< br/>
+                <br/>< br/>
                 <a href={exportAsUrl(model)}>
                     Url-encoded model -&gt;
                 </a>
-                <br />< br/>
+                <br/>< br/>
                 <a href={exportAsMinUrl(model)}>
                     MinUrl-encoded model -&gt;
                 </a>
