f44584de12b77c0cbdf7465af153cb9d6bd8e452
stackdump <myork@stackdump.com>
2025-03-29 11:32:40 -0500

    refactor model

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+  },
+  testPathIgnorePatterns: [
+    '\\.tsx$'
+  ],
 };
